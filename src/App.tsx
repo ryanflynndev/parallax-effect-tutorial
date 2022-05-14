2e7619e2687d0c1e999bae31580f737f2ed668a1
Ryan Flynn <ryanflynn44@gmail.com>
2022-05-14 15:35:08 -0400

    Finished parallax tutorial

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,67 @@
-import { Parallax, ParallaxLayer } from '@react-spring/parallax';
-import './App.css'
+import { useRef } from 'react';
+import { Parallax, ParallaxLayer, } from '@react-spring/parallax';
+import { IParallax } from '@react-spring/types';
+import moon from './assets/moon.png';
+import land from './assets/land.png';
+import cat from './assets/cat.gif';
 
 function App() {
+  const ref = useRef<IParallax>(null);
 
   return (
     <div className="App">
-      <Parallax pages={4}>
-        <ParallaxLayer>
-          <h2>Hello!</h2>
+      <Parallax pages={4} ref={ref}>
+        {/* <ParallaxLayer speed={1}>
+            <h2>Welcome to my website</h2>
+        </ParallaxLayer>
+        <ParallaxLayer offset={1} speed={0.5}>
+            <h2>Web development is fun!</h2>
+        </ParallaxLayer> */}
+
+        <ParallaxLayer
+          offset={0}
+          speed={1}
+          factor={1}
+          style={{
+            backgroundImage: `url(${moon})`,
+            backgroundSize: 'cover',
+          }}
+        />
+
+        <ParallaxLayer
+          offset={2}
+          speed={1}
+          factor={4}
+          style={{
+            backgroundImage: `url(${land})`,
+            backgroundSize: 'cover',
+          }}
+        ></ParallaxLayer>
+
+        <ParallaxLayer
+          sticky={{ start: 0.9, end: 2.5 }}
+          style={{ textAlign: 'center' }}
+        >
+          <img src={cat} />
         </ParallaxLayer>
 
-        <ParallaxLayer offset={1}>
-          <h2>I'm Ryan</h2>
+        <ParallaxLayer
+          offset={0.2}
+          speed={0.05}
+          onClick={() => ref.current.scrollTo(3)}
+        >
+          <h2>Get ready for take off!</h2>
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={3}
+          speed={2}
+          onClick={() => ref.current.scrollTo(0)}
+        >
+          <h2>Solid landing!</h2>
         </ParallaxLayer>
       </Parallax>
+      
     </div>
   )
 }
